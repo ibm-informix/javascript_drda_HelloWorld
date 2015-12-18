cc7fb1dcdc0431e287798f18c0cc8b629a5b655f
vonbarg <vonbarg@us.ibm.com>
2015-12-18 13:07:16 -0600

    Better error handling, plus other improvements

--- javascript_drda_HelloWorld.js
+++ javascript_drda_HelloWorld.js
@@ -7,7 +7,6 @@
  * 1 Create table
  * 2 Inserts
  * 2.1 Insert a single document into a table
- * 2.2 Insert multiple documents into a table
  * 3 Queries
  * 3.1 Find one document in a table that matches a query condition
  * 3.2 Find documents in a table that match a query condition
@@ -21,14 +20,24 @@
 var express = require('express');
 var app = express();
 var ibmdb = require("ibm_db");
+
+//To run locally, set the URL here.
+var URL = "";
+
+var USE_SSL = false;
+var port = process.env.VCAP_APP_PORT || 3000;
+
 var tableName = "nodeDRDA";
 var sql = "";
-var url;
-var port = process.env.VCAP_APP_PORT || 3030;
+
 var commands = [];
 
 function doEverything(res){
-	parseVcap();
+	url = URL;
+	if (url == null || url == "") {
+		url = parseVcap();
+	}
+
 	ibmdb.open(url, function(err, conn) {
 		commands.push("Connected to " + url);
 	
@@ -45,12 +54,18 @@ function doEverything(res){
 			commands.push("\n#1 Create table");
 			
 			if (err){ 
-				return console.error("error: ", err.message);
+				handleError(err, res, conn);
+				return;
 			}
 			sql = "create table if not exists " + tableName + " (name varchar(255), value integer)";
 			conn.prepare(sql,
 					function(err, statement) {
 						statement.execute(function(err, result) {
+							if (err){ 
+								handleError(err, res, conn);
+								return;
+							}
+							
 							commands.push("Create a table named: " + tableName);
 							commands.push("Create Table SQL: " + sql);
 							result.closeSync();
@@ -64,30 +79,40 @@ function doEverything(res){
 			commands.push("#2.1 Insert a single document into a table");
 			
 			if (err){ 
-				return console.error("error: ", err.message);
+				handleError(err, res, conn);
+				return;
 			}
 			sql = "insert into " + tableName + " (name, value) VALUES ('" + user1.name + "'," + user1.value + ")";
 			conn.prepare(sql,
 					function(err, statement) {
 						statement.execute(function(err, result) {
+							if (err){ 
+								handleError(err, res, conn);
+								return;
+							}
+							
 							commands.push("Create Document -> name: " + user1.name + " value: " + user1.value);
 							commands.push("Single Insert SQL: " + sql);
 							result.closeSync();
-							insertMultiple(err, conn);
+							insertAgain(err, conn);
 						});
 					});
 		}
 		
-		// not supported, must create sql statement to insert multiple
-		function insertMultiple(err) {
-			commands.push("#2.2: Insert multiple documents into a table. \n\tCurrently there is no support for this section");
+		function insertAgain(err) {
 			
 			if (err){ 
-				return console.error("error: ", err.message);
+				handleError(err, res, conn);
+				return;
 			}
 			sql = "insert into " + tableName + " (name, value) VALUES ('" + user2.name + "'," + user2.value + ")";
 			conn.prepare(sql,
 					function(err, statement) {
+						if (err){ 
+							handleError(err, res, conn);
+							return;
+						}
+				
 						statement.execute(function(err, result) {
 							result.closeSync();
 							queryById(err, conn);
@@ -100,14 +125,15 @@ function doEverything(res){
 			commands.push("#3.1 Find one document in a table that matches a query condition");
 			
 			if (err){ 
-				return console.error("error: ", err.message);
+				handleError(err, res, conn);
+				return;
 			}
 			sql = "select * from " + tableName + " where name LIKE '" + user1.name + "' LIMIT 1";
 			conn.query(sql,
 					function(err, result) {
 						commands.push("First document with name: " + user1.name);
 						commands.push("First document with name -> ", JSON.stringify(result));
-						commands.push("Query By name SQL: " + sql);
+						commands.push("Query by name, SQL: " + sql);
 						queryAllById(err, conn);
 					});
 		}
@@ -116,14 +142,15 @@ function doEverything(res){
 			commands.push("#3.2 Find documents in a table that match a query condition");
 			
 			if (err){ 
-				return console.error("error: ", err.message);
+				handleError(err, res, conn);
+				return;
 			}
 			sql = "select * from " + tableName + " where name LIKE '" + user1.name + "'";
 			conn.query(sql, 
 					function(err, result) {
 						commands.push("Find all documents with name: " + user1.name);
-						commands.push("Found Documents: ", JSON.stringify(result));
-						commands.push("Query All By name SQL: " + sql);
+						commands.push("Found documents: ", JSON.stringify(result));
+						commands.push("Query all by name, SQL: " + sql);
 					listAll(err, conn);
 				});
 		}
@@ -132,13 +159,14 @@ function doEverything(res){
 			commands.push("#3.3 Find all documents in a table");
 			
 			if (err){ 
-				return console.error("error: ", err.message);
+				handleError(err, res, conn);
+				return;
 			}
 			sql = "select * from " + tableName;
 			conn.query(sql, function(err, result) {
 				commands.push("Find all documents in table: " + tableName);
-				commands.push("Found Documents: ", JSON.stringify(result));
-				commands.push("Find All Documents SQL: " + sql);
+				commands.push("Found documents: ", JSON.stringify(result));
+				commands.push("Find all Documents, SQL: " + sql);
 				updateData(err, conn);
 			});
 		}
@@ -147,14 +175,15 @@ function doEverything(res){
 			commands.push("\n#4 Update documents in a table");
 			
 			if (err){ 
-				return console.error("error: ", err.message);
+				handleError(err, res, conn);
+				return;
 			}
 			sql = "update " + tableName + " set value = 3 where name  = '" + user1.name + "'";
 			conn.prepare(sql, 
 					function(err, statement) {
 						statement.execute(function(err, result) {
 							commands.push("Document to update: " + user1.name);
-							commands.push("Update By name SQL: " + sql);
+							commands.push("Update by name, SQL: " + sql);
 							result.closeSync();
 							deleteData(err, conn);
 						});
@@ -165,14 +194,15 @@ function doEverything(res){
 			commands.push("\n#5 Delete documents in a table");
 			
 			if (err){ 
-				return console.error("error: ", err.message);
+				handleError(err, res, conn);
+				return;
 			}
 			sql = "delete from " + tableName + " where name like '" + user1.name + "'";
 			conn.prepare(sql, 
 					function(err,statement) {
 						statement.execute(function(err, result) {
 							commands.push("Delete documents with name: " + user1.name);
-							commands.push("Delete By name SQL: " + sql);
+							commands.push("Delete by name, SQL: " + sql);
 							result.closeSync();
 							dropTable(err, conn);
 						});
@@ -183,19 +213,27 @@ function doEverything(res){
 			commands.push("\n#6 Drop a table");
 			
 			if (err){ 
-				return console.error("error: ", err.message);
+				handleError(err, res, conn);
+				return;
 			}
 			sql = "drop table " + tableName;
 			conn.prepare(sql, 
 					function(err, statement) {
 						statement.execute(function(err, result) {
 							commands.push("Drop table: " + tableName);
-							commands.push("Drop Table SQL: " + sql);
-							commands.push("\nComplete!");
+							commands.push("Drop table SQL: " + sql);
 							result.closeSync();
-							printBrowser();
+							closeConnection();
 						});
 					});
+		}
+
+		function closeConnection(){
+			conn.close(function(){
+				commands.push("Connection closed");
+				commands.push("\nComplete!");
+				printBrowser();
+			});
 		}
 		
 		function printLog(){
@@ -207,43 +245,52 @@ function doEverything(res){
 		function printBrowser(){
 			app.set('view engine', 'ejs');
 			res.render('index.ejs', {commands: commands});
-			closeConnection();
-		}
-		
-		function closeConnection(){
 			commands = [];
-			conn.close(function(){
-				console.log("Connection closed");
-			});
 		}
 		
 		createTable();
 	});
 }
 
+function handleError(err, res, conn) {
+	console.error("error: ", err.message);
+	
+	// Ensure conn object gets closed
+	if (conn) {
+		conn.close();
+	}
+	
+	// Display result
+	commands.push("ERROR: " + err.message);
+	app.set('view engine', 'ejs');
+	res.render('index.ejs', {commands: commands});
+	commands = [];
+}
+
+// VCAP_SERVICES parsing
 function parseVcap(){
-////	 vcap parsing
-	var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
-	var credentials = vcap_services['timeseriesdatabase'][0].credentials;
-	var ssl = false;
+	var serviceName = process.env.SERVICE_NAME || 'timeseriesdatabase';
+    var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+    var credentials = vcap_services[serviceName][0].credentials;
 	var drdaport;
 	var database = credentials.db;
 	var host = credentials.host;
 	var username = credentials.username;
 	var password = credentials.password;
 	
-	if (ssl){
+	if (USE_SSL) {
 		drdaport = credentials.drda_port_ssl;
-	}
-	else{
+	} else {
 	    drdaport = credentials.drda_port;  
 	}
       
 	url = "HOSTNAME=" + host + ";PORT=" + drdaport + ";DATABASE="+ database + ";PROTOCOL=TCPIP;UID=" + username +";PWD="+ password + ";";
 
-	if (ssl){
+	if (USE_SSL){
 		url += "Security=ssl";
 	}
+	
+	return url;
 }
 
 app.get('/databasetest', function(req, res) {
@@ -258,6 +305,6 @@ app.get('/', function(req, res) {
 app.listen(port,  function() {
 
 	// print a message when the server starts listening
-  console.log("Server starting on 3030");
+  console.log("Server starting on " + port);
 });
 
